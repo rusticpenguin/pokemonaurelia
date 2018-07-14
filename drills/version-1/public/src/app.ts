ecb040b74b3d921239116b414b0a0165a67bcc62
rusticpenguin <georgechios.music@gmail.com>
2018-07-13 23:28:01 -0600

    Changed colors of jumbotron and nav bar. Navbar doesn't align properly on non-desktop resolutions

--- drills/version-1/public/src/app.ts
+++ drills/version-1/public/src/app.ts
@@ -1,22 +1,22 @@
-import { Router, RouterConfiguration} from "aurelia-router";
+import { Router, RouterConfiguration } from "aurelia-router";
 import { PLATFORM } from "aurelia-framework";
 import "./styles.css";
 
 export class App {
-  router : Router;
-  message = 'Hello World!';
-    constructor(){}
+    router: Router;
+    message = 'Hello World!';
+    constructor() { }
 
-  configureRouter(config: RouterConfiguration, router: Router) : void {
+    configureRouter(config: RouterConfiguration, router: Router): void {
 
-    config.title = "Ultimate Battle";
-    config.options.root = "/";
-    config.options.pushState = true;
-    config.map([
-      {route: ["", "home"], name: "home", moduleId: PLATFORM.moduleName("./home"), nav: true, title: "Home"},
-      {route: "about", name: "about", moduleId: PLATFORM.moduleName("./about"), nav: true, title: "About"}
-    ]);
+        config.title = "Ultimate Battle";
+        config.options.root = "/";
+        config.options.pushState = true;
+        config.map([
+            { route: ["", "home"], name: "home", moduleId: PLATFORM.moduleName("./home"), nav: true, title: "Home" },
+            { route: "about", name: "about", moduleId: PLATFORM.moduleName("./about"), nav: true, title: "About" }
+        ]);
 
-    this.router = router;
-  }
+        this.router = router;
+    }
 }
--- drills/version-1/public/src/main.ts
+++ drills/version-1/public/src/main.ts
@@ -2,9 +2,9 @@
 // we want font-awesome to load as soon as possible to show the fa-spinner
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-import {Aurelia} from 'aurelia-framework'
+import { Aurelia } from 'aurelia-framework'
 import environment from './environment';
-import {PLATFORM} from 'aurelia-pal';
+import { PLATFORM } from 'aurelia-pal';
 import * as Bluebird from 'bluebird';
 
 
@@ -12,24 +12,24 @@ import * as Bluebird from 'bluebird';
 Bluebird.config({ warnings: { wForgottenReturn: false } });
 
 export function configure(aurelia: Aurelia) {
-  aurelia.use
-    .standardConfiguration()
-    .feature(PLATFORM.moduleName('resources/index'));
+    aurelia.use
+        .standardConfiguration()
+        .feature(PLATFORM.moduleName('resources/index'));
 
-  // Uncomment the line below to enable animation.
-  // aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
-  // if the css animator is enabled, add swap-order="after" to all router-view elements
+    // Uncomment the line below to enable animation.
+    // aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
+    // if the css animator is enabled, add swap-order="after" to all router-view elements
 
-  // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
-  // aurelia.use.plugin(PLATFORM.moduleName('aurelia-html-import-template-loader'));
+    // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
+    // aurelia.use.plugin(PLATFORM.moduleName('aurelia-html-import-template-loader'));
 
-  if (environment.debug) {
-    aurelia.use.developmentLogging();
-  }
+    if (environment.debug) {
+        aurelia.use.developmentLogging();
+    }
 
-  if (environment.testing) {
-    aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
-  }
+    if (environment.testing) {
+        aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
+    }
 
-  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
+    aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
 }
